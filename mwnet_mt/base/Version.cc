b7cfe2ecc679c928cffe8c26d9e7ceffea121713
JIANGHB <46246299+cocoj521@users.noreply.github.com>
2023-07-18 16:46:14 +0800

    刷新版本号 2.0.0.2

--- mwnet_mt/base/Version.cc
+++ mwnet_mt/base/Version.cc
@@ -2,4 +2,4 @@
 // that can be found in the License file.
 #include <string>
 
-const std::string strVer = "lib_version:2.0.0.1 2020-08-27 16:37";
+const std::string strVer = "lib_version:2.0.0.2 2023-07-18 16:38";
